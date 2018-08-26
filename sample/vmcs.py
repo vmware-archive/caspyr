9f7f420ae044aac8c3b1a1c9726c4e7c1e952089
Grant Orchard <grant.a.orchard@gmail.com>
2018-08-25 17:39:55 -0700

    Add cleanup details for zones and projects.

--- sample/vmcs.py
+++ sample/vmcs.py
@@ -367,7 +367,7 @@ class CloudZone(object):
         try:
             r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
             j = r.json()
-            print(j)
+            r.raise_for_status()
             return j
         except requests.exceptions.HTTPError as e:
             print(e)
@@ -398,10 +398,10 @@ class CloudZone(object):
     def delete(session, id):
         uri = f'/iaas/zones/{id}'
         try:
-            requests.delete(f'{session.baseurl}{uri}', headers = session.headers)
+            r = requests.delete(f'{session.baseurl}{uri}', headers = session.headers)
+            r.raise_for_status()
         except requests.exceptions.HTTPError as e:
             print(e)
-            sys.exit(1)
 
 class Deployment(object):
     """
@@ -527,7 +527,8 @@ class StorageProfile(object):
     def delete_vsphere(session, id):
         uri = f'/iaas/storage-profiles-vsphere/{id}'
         try:
-            requests.delete(f'{session.baseurl}{uri}', headers = session.headers)
+            r = requests.delete(f'{session.baseurl}{uri}', headers = session.headers)
+            r.raise_for_status()
         except requests.exceptions.HTTPError as e:
             print(e)
             sys.exit(1)
@@ -537,11 +538,13 @@ class ImageMapping(object):
     @staticmethod
     def list(session):
         uri = '/iaas/image-profiles'
-        r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
-        j = r.json()
-        if r.status_code != 200:
-            print('Unable to list image profiles, status code',r.status_code)
-        return j
+        try:
+            r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
+            r.raise_for_status()
+            j = r.json()
+            return j
+        except requests.exceptions.HTTPError as e:
+            print(e)
 
     @staticmethod
     def delete(session, i):
@@ -581,7 +584,7 @@ class Org():
             "usernames": [
                 username
             ],
-            "mandatoryOrgRole": "org_owner",
+            "mandatoryOrgRole": "org_member",
             "addOnRoles": [],
             "orgServicesRoles": [
                 {
@@ -629,7 +632,6 @@ class Org():
             r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
             r.raise_for_status()
             j=r.json()
-            #print('User',username,'invited to org',org_id)
             return j
         except requests.exceptions.HTTPError as e:
             print(e)
@@ -639,7 +641,17 @@ class Org():
 class CodeStream(object):
     @staticmethod
     def endpoint_list(session):
-        pass
+        uri = f'/pipeline/api/endpoints'
+        try:
+            data = list()
+            r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
+            r.raise_for_status()
+            j=r.json()
+            print(j)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            sys.exit(1)
+        return data
 
     @staticmethod
     def endpoint_delete(session, id):
@@ -665,13 +677,27 @@ class CodeStream(object):
     def pipeline_status(session, id):
         pass
 
+class ServiceBroker(object):
+    @staticmethod
+    def sources_list(session):
+        uri = f'/library/api/admin/sources'
+        try:
+            data = list()
+            r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
+            r.raise_for_status()
+            j=r.json()
+            print(j)
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            sys.exit(1)
+        return data
 
 
 
 
 """
 {
-    "orgRoleName": "org_owner",
+    "orgRoleName": "org_member",
     "orgRoleNames": [ "" ],
     "serviceRolesDtos": [ {
         "serviceDefinitionLink": "/csp/gateway/slc/api/definitions/external/Zy924mE3dwn2ASyVZR0Nn7lupeA_",
