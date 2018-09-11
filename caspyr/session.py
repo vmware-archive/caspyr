da50da6a46b6380dc56894de39a25f091dc21830
Grant Orchard <grant.a.orchard@gmail.com>
2018-09-11 14:33:34 +1000

    Added the _request method for DRYer code.

--- caspyr/session.py
+++ caspyr/session.py
@@ -29,6 +29,51 @@ class Session(object):
                 print(e)
                 sys.exit(1)
 
-    @classmethod
-    def _request(cls, session):
-        pass
+    def _request(self, uri, request_method='GET', data=None, **kwargs):
+        """
+        Credits: Russell Pope
+        Generic requestor method for all of the HTTP methods. This gets invoked by pretty much everything in the API.
+        You can also use it to do anything not yet implemented in the API.
+
+        :param uri: The complete uri for the requested resource. You should include the leading /
+        :param request_method: An HTTP method that is either PUT, POST, PATCH, DELETE or GET
+        :param payload: Used to store a resource that is used in either POST or PUT operations
+        :param kwargs: Unused currently
+        :return: A python dictionary containing the response JSON
+
+        """
+
+        if request_method == "PUT" or "POST" or "PATCH" and data:
+            if type(data) == dict:
+                data = json.dumps(json)
+
+            r = requests.request(request_method,
+                                 url=uri,
+                                 headers=self.headers,
+                                 json=data)
+
+            if not r.ok:
+                raise requests.exceptions.HTTPError('HTTP error. Status code was:', r.status_code, r.content)
+
+            return r.json()
+
+        elif request_method == "GET":
+            r = requests.request(request_method,
+                                 url=uri,
+                                 headers=self.headers)
+
+            if not r.ok:
+                uri_code = f'Target uri: {r.uri}, Status Code: {r.status_code}'
+                raise requests.exceptions.HTTPError('HTTP error. Status and content:',uri_code , r.content)
+
+            return r.json()
+
+        elif request_method == "DELETE":
+            r = requests.request(request_method,
+                                 url=uri,
+                                 headers=self.headers)
+
+            if not r.ok:
+                raise requests.exceptions.HTTPError('HTTP error. Status code was:', r.status_code)
+
+            return r.json()
