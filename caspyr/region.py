8323ef22b368820dbae7a52ec44b9f018676d35a
Grant Orchard <grant.a.orchard@gmail.com>
2018-09-13 21:03:10 +1000

    Initial commit

---
+++ caspyr/region.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+import sys
+
+class Region(object):
+    """
+    Class for Region methods.
+    Used to discover regions for all fabric constructs (images, mappings, networks and storage.)
+    """
+    def _init__(self, region):
+        self.external_region_id = region['externalRegionId']
+        self.id = region['id']
+        self.self_link = region['selfLink']
+        self.updated_at = region['updatedAt']
+        self._links = region['_links']
+
+    @staticmethod
+    def list(session):
+        uri = f'/iaas/regions'
+        j = session._request(f'{session.baseurl}{uri}')
+        return j['content']
+
+    @classmethod
+    def describe(cls, session, id):
+        uri = f'/iaas/regions/{id}'
+        j = session._request(f'{session.baseurl}{uri}')
+        return cls(j)
