a6fd3a8c6b7a301f0a930044522be1a26bd74e33
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-11 21:13:46 +1100

    Adding logging

--- caspyr/session.py
+++ caspyr/session.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sys
+import logging
 
 class Session(object):
     """
@@ -9,13 +10,14 @@ class Session(object):
 
     Requires refresh token from VMware Cloud Services portal to instantiate.
     """
-    def __init__(self, auth_token):
+    def __init__(self, auth_token, log_level):
         self.token = 'Bearer '+auth_token
         self.headers = {'Content-Type':'application/json','authorization': self.token}
         self.baseurl = 'https://api.mgmt.cloud.vmware.com'
+        self.log_level = log_level
 
     @classmethod
-    def login(self, refresh_token):
+    def login(self, refresh_token, log_level='WARNING'):
             baseurl = 'https://api.mgmt.cloud.vmware.com'
             uri = '/iaas/login'
             headers = {'Content-Type':'application/json'}
@@ -24,56 +26,68 @@ class Session(object):
                 r = requests.post(f'{baseurl}{uri}', headers = headers, data = payload)
                 print('Login successful.')
                 auth_token = r.json()['token']
-                return self(auth_token)
+                return self(auth_token, log_level.upper())
             except requests.exceptions.HTTPError as e:
                 print(e)
                 sys.exit(1)
 
     def _request(self, url, request_method='GET', payload=None, **kwargs):
         """
-        Credits: Russell Pope
-        Generic requestor method for all of the HTTP methods. This gets invoked by pretty much everything in the API.
-        You can also use it to do anything not yet implemented in the API.
-
-        :param uri: The complete uri for the requested resource. You should include the leading /
+        Inspired by the work of Russell Pope.
+        :param url: The complete uri for the requested resource. You should include the leading /
         :param request_method: An HTTP method that is either PUT, POST, PATCH, DELETE or GET
         :param payload: Used to store a resource that is used in either POST or PUT operations
         :param kwargs: Unused currently
         :return: A python dictionary containing the response JSON
 
         """
+        numeric_level = getattr(logging, self.log_level, None)
+        if not isinstance(numeric_level, int):
+            raise ValueError('Invalid log level: %s' % self.log_level)
+        logging.basicConfig(level=numeric_level)
 
         if request_method == "PUT" or "POST" or "PATCH" and payload:
             if type(payload) == dict:
                 payload = json.dumps(payload)
 
-            r = requests.request(request_method,
-                                 url=url,
-                                 headers=self.headers,
-                                 data=payload)
+            try:
+                r = requests.request(request_method,
+                                    url=url,
+                                    headers=self.headers,
+                                    data=payload)
 
-            if not r.ok:
-                raise requests.exceptions.HTTPError('HTTP error. Status code was:', r.status_code, r.content)
+                logging.info(f'{request_method} to {url} with payload {payload}')
+                logging.info(f'request response code {r.status_code}')
+                logging.info(f'request content {json.dumps(r.json(), indent=2)}')
 
-            return r.json()
+                r.raise_for_status()
+                return r.json()
+
+            except requests.exceptions.HTTPError as e:
+                logging.error('request failed with {e}')
+                print (e)
 
         elif request_method == "GET":
-            r = requests.request(request_method,
-                                 url=url,
-                                 headers=self.headers)
-
-            if not r.ok:
-                uri_code = f'Target uri: {r.url}, Status Code: {r.status_code}'
-                raise requests.exceptions.HTTPError('HTTP error. Status and content:',uri_code , r.content)
-
-            return r.json()
+            try:
+                r = requests.request(request_method,
+                                    url=url,
+                                    headers=self.headers)
+                logging.info(f'{request_method} to {url}')
+                logging.info(f'request response code {r.status_code}')
+                logging.info(f'request content {r.json}')
+                r.raise_for_status()
+                return r.json()
+            except requests.exceptions.HTTPError as e:
+                logging.error('request failed with {e}')
+                print (e)
 
         elif request_method == "DELETE":
-            r = requests.request(request_method,
-                                 url=url,
-                                 headers=self.headers)
-
-            if not r.ok:
-                raise requests.exceptions.HTTPError('HTTP error. Status code was:', r.status_code)
-
-            return r.json()
+            try:
+                r = requests.request(request_method,
+                                    url=url,
+                                    headers=self.headers)
+                r.raise_for_status()
+                return r.json()
+            except requests.exceptions.HTTPError as e:
+                logging.error('request failed with {e}')
+                print (e)
