aa7f87e4a395f603d30edd7782a279cec5241276
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-23 16:35:13 +1100

    Fixed parsing of HTTP method inputs for Session._request.

--- caspyr/session.py
+++ caspyr/session.py
@@ -24,12 +24,13 @@ class Session(object):
             payload = json.dumps({"refreshToken": refresh_token })
             try:
                 r = requests.post(f'{baseurl}{uri}', headers = headers, data = payload)
-                print('Login successful.')
+                r.raise_for_status()
                 auth_token = r.json()['token']
                 return self(auth_token, log_level.upper())
             except requests.exceptions.HTTPError as e:
                 print(e)
-                sys.exit(1)
+                raise e
+
 
     def _request(self, url, request_method='GET', payload=None, **kwargs):
         """
@@ -38,7 +39,7 @@ class Session(object):
         :param request_method: An HTTP method that is either PUT, POST, PATCH, DELETE or GET
         :param payload: Used to store a resource that is used in either POST or PUT operations
         :param kwargs: Unused currently
-        :return: A python dictionary containing the response JSON
+        :return: The response JSON
 
         """
         numeric_level = getattr(logging, self.log_level, None)
@@ -46,7 +47,7 @@ class Session(object):
             raise ValueError('Invalid log level: %s' % self.log_level)
         logging.basicConfig(level=numeric_level)
 
-        if request_method == "PUT" or "POST" or "PATCH" and payload:
+        if request_method in ('PUT', 'POST', 'PATCH') and payload:
             if type(payload) == dict:
                 payload = json.dumps(payload)
 
@@ -58,16 +59,16 @@ class Session(object):
 
                 logging.info(f'{request_method} to {url} with payload {payload}')
                 logging.info(f'request response code {r.status_code}')
-                logging.info(f'request content {json.dumps(r.json(), indent=2)}')
+                logging.info(f'request content {r.json()}')
 
                 r.raise_for_status()
                 return r.json()
 
             except requests.exceptions.HTTPError as e:
-                logging.error('request failed with {e}')
-                print (e)
+                logging.error(f'request failed with {e}')
+                raise e
 
-        elif request_method == "GET":
+        elif request_method == 'GET':
             try:
                 r = requests.request(request_method,
                                     url=url,
@@ -78,16 +79,16 @@ class Session(object):
                 r.raise_for_status()
                 return r.json()
             except requests.exceptions.HTTPError as e:
-                logging.error('request failed with {e}')
+                logging.error(f'request failed with {e}')
                 print (e)
 
-        elif request_method == "DELETE":
+        elif request_method == 'DELETE':
             try:
                 r = requests.request(request_method,
                                     url=url,
                                     headers=self.headers)
                 r.raise_for_status()
-                return r.json()
+                return r.status_code
             except requests.exceptions.HTTPError as e:
-                logging.error('request failed with {e}')
+                logging.error(f'request failed with {e}')
                 print (e)
