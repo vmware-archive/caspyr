2cc93ba2f495cf8584ce045bc1490b3a729ed2dc
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-25 21:53:18 +1100

    Adding fabric import

--- caspyr/__init__.py
+++ caspyr/__init__.py
@@ -5,6 +5,7 @@ from .request import Request
 from .region import Region
 from .deployment import Deployment
 from .project import Project
+from .fabric import Image
 from .mapping import FlavorMapping, ImageMapping, StorageProfileAWS, StorageProfileAzure, StorageProfilevSphere, NetworkFabric, NetworkProfile, Network
 from .zone import CloudZone
-from .codestream import CodeStream  
+from .codestream import CodeStream
