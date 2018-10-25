2326d5247ad44796c0ab47d17bf1ade6c04e8c09
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-25 16:52:59 +1100

    Added handling for spaces in the filter query of describe_by_name

--- caspyr/region.py
+++ caspyr/region.py
@@ -8,10 +8,9 @@ class Region(object):
     Class for Region methods.
     Used to discover regions for all fabric constructs (images, mappings, networks and storage.)
     """
-    def _init__(self, region):
+    def __init__(self, region):
         self.external_region_id = region['externalRegionId']
         self.id = region['id']
-        self.self_link = region['selfLink']
         self.updated_at = region['updatedAt']
         self._links = region['_links']
 
@@ -24,11 +23,9 @@ class Region(object):
     @classmethod
     def describe(cls, session, id):
         uri = f'/iaas/regions/{id}'
-        j = session._request(f'{session.baseurl}{uri}')
-        return cls(j)
+        return cls(session._request(f'{session.baseurl}{uri}'))
 
-    @classmethod
-    def describe_by_name(cls, session, name):
-        uri = f'/iaas/regions/?$filter=externalRegionId eq {name}'
+    def describe_by_name(self, session, name):
+        uri = f'/iaas/regions/?$filter=(externalRegionId eq \'{name}\')'
         j = session._request(f'{session.baseurl}{uri}')
         return j
