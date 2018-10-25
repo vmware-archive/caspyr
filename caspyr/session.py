6d147071b31a1b3b2f4f69429a68f9bc804c5228
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-25 16:51:48 +1100

    moved logging to _requests

--- caspyr/session.py
+++ caspyr/session.py
@@ -10,14 +10,13 @@ class Session(object):
 
     Requires refresh token from VMware Cloud Services portal to instantiate.
     """
-    def __init__(self, auth_token, log_level):
+    def __init__(self, auth_token):
         self.token = 'Bearer '+auth_token
         self.headers = {'Content-Type':'application/json','authorization': self.token}
         self.baseurl = 'https://api.mgmt.cloud.vmware.com'
-        self.log_level = log_level
 
     @classmethod
-    def login(self, refresh_token, log_level='WARNING'):
+    def login(self, refresh_token):
             baseurl = 'https://api.mgmt.cloud.vmware.com'
             uri = '/iaas/login'
             headers = {'Content-Type':'application/json'}
@@ -26,13 +25,13 @@ class Session(object):
                 r = requests.post(f'{baseurl}{uri}', headers = headers, data = payload)
                 r.raise_for_status()
                 auth_token = r.json()['token']
-                return self(auth_token, log_level.upper())
+                return self(auth_token)
             except requests.exceptions.HTTPError as e:
                 print(e)
                 raise e
 
 
-    def _request(self, url, request_method='GET', payload=None, **kwargs):
+    def _request(self, url, request_method='GET', payload=None, log_level='WARNING', **kwargs):
         """
         Inspired by the work of Russell Pope.
         :param url: The complete uri for the requested resource. You should include the leading /
@@ -42,9 +41,9 @@ class Session(object):
         :return: The response JSON
 
         """
-        numeric_level = getattr(logging, self.log_level, None)
+        numeric_level = getattr(logging, log_level, None)
         if not isinstance(numeric_level, int):
-            raise ValueError('Invalid log level: %s' % self.log_level)
+            raise ValueError('Invalid log level: %s' % log_level)
         logging.basicConfig(level=numeric_level)
 
         if request_method in ('PUT', 'POST', 'PATCH') and payload:
@@ -60,12 +59,11 @@ class Session(object):
                 logging.info(f'{request_method} to {url} with payload {payload}')
                 logging.info(f'request response code {r.status_code}')
                 logging.info(f'request content {r.json()}')
-
                 r.raise_for_status()
                 return r.json()
 
             except requests.exceptions.HTTPError as e:
-                logging.error(f'request failed with {e}')
+                logging.error(f'request failed with {e}. {r.json()}')
                 raise e
 
         elif request_method == 'GET':
@@ -87,6 +85,9 @@ class Session(object):
                 r = requests.request(request_method,
                                     url=url,
                                     headers=self.headers)
+                logging.info(f'{request_method} to {url}')
+                logging.info(f'request response code {r.status_code}')
+                logging.info(f'request content {r.json}')
                 r.raise_for_status()
                 return r.status_code
             except requests.exceptions.HTTPError as e:
