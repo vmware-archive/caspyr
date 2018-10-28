1d51803ecf9dc3e61043006f54355021ea3bf826
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-29 10:29:35 +1100

    Added User Module and Flavor Mapping class

--- caspyr/__init__.py
+++ caspyr/__init__.py
@@ -5,7 +5,8 @@ from .request import Request
 from .region import Region
 from .deployment import Deployment
 from .project import Project
-from .fabric import Image
+from .fabric import Image, AzureStorageAccount
 from .mapping import FlavorMapping, ImageMapping, StorageProfileAWS, StorageProfileAzure, StorageProfilevSphere, NetworkFabric, NetworkProfile, Network
 from .zone import CloudZone
 from .codestream import CodeStream
+from .user import User
