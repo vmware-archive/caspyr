eac09d68ccd7448649b2e6cc7c4c9e7ac9636d53
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-30 21:36:19 +1100

    Added StorageProfile.list and .delete methods.

--- caspyr/mapping.py
+++ caspyr/mapping.py
@@ -28,15 +28,24 @@ class StorageProfile(metaclass=ABCMeta):
             self.vsphere_storage_policies = profile['vsphereStoragePolicies']
         except KeyError: pass
 
+    @staticmethod
+    def list(session):
+        uri = '/iaas/storage-profiles'
+        return session._request(f'{session.baseurl}{uri}')['content']
+
+    @staticmethod
+    def delete(session, id):
+        uri = f'/iaas/storage-profiles/{id}'
+        return session._request(f'{session.baseurl}{uri}', request_method='DELETE')
+
 class StorageProfileAzure(StorageProfile):
     def __init__(self, storageprofile):
         super().__init__(storageprofile)
 
     @staticmethod
     def list(session):
-        uri = '/iaas/storage-profiles-azure'
-        j = session._request(f'{session.baseurl}{uri}')
-        return j['content']
+        uri = '/iaas/storage-profiles-azure/'
+        return session._request(f'{session.baseurl}{uri}')['content']
 
     @classmethod
     def describe(cls, session, id):
@@ -87,13 +96,13 @@ class StorageProfileAWS(StorageProfile):
 
     @staticmethod
     def list(session):
-        uri = '/iaas/storage-profiles-aws'
-        j = session._request(f'{session.baseurl}{uri}')
-        return j['content']
+        uri = '/iaas/storage-profiles-aws/'
+        return session._request(f'{session.baseurl}{uri}')['content']
+
 
     @staticmethod
     def delete(session, id):
-        uri = '/iaas/storage-profiles-aws/{id}'
+        uri = f'/iaas/storage-profiles-aws/{id}'
         return session._request(f'{session.baseurl}{uri}', request_method='DELETE')
 
     @classmethod
@@ -215,7 +224,7 @@ class FlavorMapping(object):
         self.organization_id=mapping['organizationId']
         self.external_region_id=mapping['externalRegionId']
         self._links=mapping['_links']
-        self.image_mappings=mapping['flavorMappings']
+        self.flavor_mappings=mapping['flavorMappings']
 
     @staticmethod
     def list(session):
@@ -247,69 +256,6 @@ class FlavorMapping(object):
         }
         return cls(session._request(f'{session.baseurl}{uri}', request_method='POST', payload=payload))
 
-class NetworkFabric(object):
-    """
-
-    """
-    def __init__(self, network):
-        self.external_region_id = network['externalRegionId']
-        self.name = network['name']
-        self.id = network['id']
-        self.self_link = network['selfLink']
-        self.created_at = network['createdAt']
-        self.updated_at = network['updatedAt']
-        self.organization_id = network['organizationId']
-        self._links = network['_links']
-        try:
-            self.is_public = network['isPublic']
-        except KeyError: pass
-        try:
-            self.is_default = network['isDefault']
-        except KeyError: pass
-        try:
-            self.cidr = network['cidr']
-        except KeyError: pass
-
-    @staticmethod
-    def list(session):
-        uri = '/iaas/fabric-networks'
-        try:
-            r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
-            j = r.json()
-            data = list()
-            for i in j['content']:
-                data.append({i['externalRegionId'], i['name'], i['id']})
-            return data
-        except requests.exceptions.HTTPError as e:
-            print(e)
-
-    @staticmethod
-    def list_for_region(session, region="*"):
-        uri = f'/iaas/fabric-networks?$filter=externalRegionId eq {region}'
-        try:
-            r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
-            j = r.json()
-            data = list()
-            for i in j['content']:
-                data.append({i['externalRegionId'], i['name'], i['id']})
-            return data
-        except requests.exceptions.HTTPError as e:
-            print(e)
-
-    @classmethod
-    def describe(cls, session, id):
-        uri = f'/iaas/fabric-networks/{id}'
-        try:
-            r = requests.get(f'{session.baseurl}{uri}', headers = session.headers)
-            j = r.json()
-            return cls(j)
-        except requests.exceptions.HTTPError as e:
-            print(e)
-
-    @classmethod
-    def describe_by_name(cls, session):
-        pass
-
 class NetworkProfile(object):
     """
     Class for Network Profile methods.
@@ -321,7 +267,6 @@ class NetworkProfile(object):
         self.tags = network['tags']
         self.name = network['name']
         self.id = network['id']
-        self.self_link = network['selfLink']
         self.updated_at = network['updatedAt']
         self.organization_id = network['organizationId']
         self._links = network['_links']
@@ -333,29 +278,62 @@ class NetworkProfile(object):
         return j['content']
 
     @classmethod
-    def create(cls, session, name, region, networks=[], tags=[]):
-        """
-        This method requires you to peel the onion a bit.
-        It is worth noting that network profiles are scoped to a given region.
-        To get the region info you need, first off determine the Cloud Account that has the networks you want to add to your network profile.
-        Perform a CloudAccount.describe(session, id) against that account, and return the cls.enabled_region_ids.
-        Use the enabled_region_id value for the region you want to configure networks for.
-        :param session: type: object. Generated from Session.login(token).
-        :param name: type: string. The desired name of the network profile.
-        :param networks: type: array. A list of networks and their ids can be found using NetworkFabric.list_for_region(session, region). The region itself can be found by CloudAccount.describe
-        :param region: type:string. The id is generated from ProvisioningRegion.find_by_region_id(session, )
-        :returns: this is a description of what is returned
-        :raises keyError: raises an exception
+    def create(cls,
+               session,
+               name,
+               region_id,
+               network_ids,
+               isolation_type=None,
+               security_group_ids=None,
+               isolation_network_domain_id=None,
+               isolation_network_domain_cidr=None,
+               isolation_external_network_fabric_id=None,
+               isolated_network_cidr_prefix=None,
+               description=None,
+               tags=None):
+        """[summary]
+        :param session: [description]
+        :type session: [type]
+        :param name: [description]
+        :type name: [type]
+        :param region_id: [description]
+        :type region_id: [type]
+        :param network_ids: [description]
+        :type network_ids: [type]
+        :param isolation_type: [description], defaults to None
+        :param isolation_type: [type], optional
+        :param security_group_ids: [description], defaults to None
+        :param security_group_ids: [type], optional
+        :param isolation_network_domain_id: [description], defaults to None
+        :param isolation_network_domain_id: [type], optional
+        :param isolation_network_domain_cidr: [description], defaults to None
+        :param isolation_network_domain_cidr: [type], optional
+        :param isolation_external_network_fabric_id: [description], defaults to None
+        :param isolation_external_network_fabric_id: [type], optional
+        :param isolated_network_cidr_prefix: [description], defaults to None
+        :param isolated_network_cidr_prefix: [type], optional
+        :param description: [description], defaults to None
+        :param description: [type], optional
+        :param tags: [description], defaults to None
+        :type tags: List, optional
+        :return: [description]
+        :rtype: [type]
         """
         uri = '/iaas/network-profiles'
-        data = {
+        payload = {
             "name": name,
-            "regionId": region,
-            "fabricNetworkIds" : networks,
+            "description": description,
+            "regionId": region_id,
+            "fabricNetworkIds" : network_ids,
+            "isolationType" : isolation_type,
+            "securityGroupIds" : security_group_ids,
+            "isolationNetworkDomainId": isolation_network_domain_id,
+            "isolationNetworkDomainCIDR": isolation_network_domain_cidr,
+            "isolationExternalFabricNetworkId": isolation_external_network_fabric_id,
+            "isolatedNetworkCIDRPrefix": isolated_network_cidr_prefix,
             "tags": tags
         }
-        return session._request(f'{session.baseurl}{uri}', request_method='POST', json=data)
-
+        return cls(session._request(f'{session.baseurl}{uri}', request_method='POST', payload=payload))
 
     @staticmethod
     def delete(session, id):
@@ -366,23 +344,3 @@ class NetworkProfile(object):
     def describe(cls, session, id):
         uri = f'/iaas/network-profiles/{id}'
         return cls(session._request(f'{session.baseurl}{uri}'))
-
-class Network(object):
-    def __init__(self, network):
-        pass
-
-    @staticmethod
-    def list(session):
-        uri = f'/iaas/networks'
-        j = session._request(f'{session.baseurl}{uri}')
-        return j['content']
-
-    @classmethod
-    def describe(cls, session, id):
-        uri = f'/iaas/networks/{id}'
-        return cls(session._request(f'{session.baseurl}{uri}'))
-
-    @staticmethod
-    def delete(session, id):
-        uri = f'/iaas/networks/{id}'
-        return session._request(f'{session.baseurl}{uri}', request_method='DELETE')
