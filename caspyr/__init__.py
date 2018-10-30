5358217792f86d948340daa5f23225d86a6cccbf
Grant Orchard <grant.a.orchard@gmail.com>
2018-10-30 21:41:05 +1100

    Added class imports from iaas module.

--- caspyr/__init__.py
+++ caspyr/__init__.py
@@ -1,12 +1,13 @@
 from .session import Session
 from .blueprint import Blueprint
-from .cloudaccount import CloudAccount, CloudAccountAws, CloudAccountAzure, CloudAccountvSphere
+from .cloudaccount import CloudAccount, CloudAccountAws, CloudAccountAzure, CloudAccountvSphere, CloudAccountNSXT
 from .request import Request
 from .region import Region
 from .deployment import Deployment
 from .project import Project
-from .fabric import Image, AzureStorageAccount
-from .mapping import FlavorMapping, ImageMapping, StorageProfileAWS, StorageProfileAzure, StorageProfilevSphere, NetworkFabric, NetworkProfile, Network
+from .fabric import Image, AzureStorageAccount, NetworkFabric
+from .mapping import FlavorMapping, ImageMapping, StorageProfile, StorageProfileAWS, StorageProfileAzure, StorageProfilevSphere, NetworkProfile
 from .zone import CloudZone
 from .codestream import CodeStream
 from .user import User
+from .iaas import Machine, Network
