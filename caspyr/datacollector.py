f41ea4035ab186edf8b6f6d6b9aa03cbc7db87d0
Grant Orchard <grant.a.orchard@gmail.com>
2018-11-04 13:13:31 +0100

    initial commit

---
+++ caspyr/datacollector.py
@@ -0,0 +1,24 @@
+class DataCollector(object):
+    def __init__(self, rdc):
+        pass
+
+    @staticmethod
+    def list(session):
+        uri = '/iaas/data-collectors/'
+        data = []
+        j = session._request(f'{session.baseurl}{uri}')['content']
+        for i in j:
+            content = {}
+            content['id'] = i['dcId']
+            content['name'] = i['name']
+            data.append(content)
+        return data
+
+    @classmethod
+    def describe(cls):
+        pass
+
+    @staticmethod
+    def delete(session, id):
+        uri = f'/iaas/data-collectors/{id}'
+        return session._request(f'{session.baseurl}{uri}', request_method='DELETE')
