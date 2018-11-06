5f2c401179e339633abc08019f17e542ba5801ee
Grant Orchard <grant.a.orchard@gmail.com>
2018-11-06 06:33:48 +0100

    Added DataCollector

--- caspyr/__init__.py
+++ caspyr/__init__.py
@@ -5,9 +5,10 @@ from .request import Request
 from .region import Region
 from .deployment import Deployment
 from .project import Project
-from .fabric import Image, AzureStorageAccount, NetworkFabric
+from .fabric import Image, AzureStorageAccount, NetworkFabric, Flavor
 from .mapping import FlavorMapping, ImageMapping, StorageProfile, StorageProfileAWS, StorageProfileAzure, StorageProfilevSphere, NetworkProfile
 from .zone import CloudZone
 from .codestream import CodeStream
 from .user import User
 from .iaas import Machine, Network
+from .datacollector import DataCollector
