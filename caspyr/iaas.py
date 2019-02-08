04efe537131b2f90195d66375caf293d1989f5f7
Grant Orchard <gorchard@vmware.com>
2019-02-09 10:17:46 +1100

    Added get_ip method

--- caspyr/iaas.py
+++ caspyr/iaas.py
@@ -31,6 +31,39 @@ class Machine(object):
         j = session._request(f'{session.baseurl}{uri}')
         return j['content']
 
+    @classmethod
+    def describe(cls, session, id):
+        uri = f'/iaas/machines/{id}'
+        return session._request(f'{session.baseurl}{uri}')
+
+    @staticmethod
+    def get_ip(session,
+               machine_id,
+               nic_index=0,
+               address_index=0
+               ):
+        """Get the internal address of a machine.
+        This method defaults to returning the first IP address of the first
+        network interface on the provided machine id. You can adjust the
+        interface and address index with the nic_index and address_index
+        values.
+
+        :param session: The Session object
+        :type session: cls
+        :param machine_id: The resource id of the machine
+        :type machine_id: str
+        :param nic_index: The index of the network interface for which you
+            want to retrieve the address. Defaults to 0.
+        :type nic_index: int
+        :param address_index: The index of the address which you want to
+            retrieve.
+        Defaults to 0.
+        """
+        j = Machine.describe(session, machine_id)
+        uri = j["_links"]["network-interfaces"]["hrefs"][nic_index]
+        i = session._request(f'{session.baseurl}{uri}')
+        return i['addresses'][address_index]
+
     @staticmethod
     def delete(session, id):
         uri = f'/iaas/machines/{id}'
