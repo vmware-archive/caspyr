383ef3173f452b72ef2cb289de60e2a39e09f01b
Cody De Arkland <codydearkland@gmail.com>
2019-05-27 19:35:26 -0700

    added additional help text for methods

--- caspyr/integration.py
+++ caspyr/integration.py
@@ -5,12 +5,25 @@
 
 
 class Integration: 
+    """
+    Class for methods related to Cloud Assembly Integrations.
+    :method list: Returns an array of all endpoint resources that are
+    which includes Cloud Accounts and Integrations.
+    :method delete: Deletes the resource endpoint.
+    """
     def __init__(self, integration):
         self.name = integration['name']
         self.id = integration['id']
 
     @staticmethod
     def list(session):
+        """Retrieves list of all resource endpoints.
+
+        :param session: The session object.
+        :type session: object
+        :return: A list of resource endpoints.
+        :rtype: list
+        """
         uri = '/provisioning/uerp/resources/endpoints?expand'
         j = session._request(f'{session.baseurl}{uri}')
         endpoints = []
@@ -25,24 +38,57 @@ class Integration:
 
     @staticmethod
     def delete(session,resourceLink):
+        """Deletes a resource endpoint/integration.
+
+        :param session: The session object.
+        :type session: object
+        :param resourceLink: The resourceLink (URL path) for
+        the item in for deletion
+        :type id: string
+        :return: Success/Failure.
+        :rtype: status code
+        """
         uri = f'/provisioning/uerp/provisioning/mgmt/endpoints{resourceLink}'
         return session._request(f'{session.baseurl}{uri}',
                                 request_method='DELETE')
 
 
 class Source: 
+    """
+    Class for methods related to integration sources.
+    :method list: Returns an array of all integration sources from
+    bound accounts and integrations.
+    :method delete: Deletes an associated integration data source.
+    """
     def __init__(self, source):
         self.name = source['name']
         self.id = source['name']
 
     @staticmethod
     def list(session):
+        """Retrieves list of all integration source within a
+        Organization
+
+        :param session: The session object.
+        :type session: object
+        :return: A list of sources.
+        :rtype: list
+        """
         uri = '/content/api/sources/'
         j = session._request(f'{session.baseurl}{uri}')
         return j['content']
 
     @staticmethod
     def delete(session,id):
+        """Deletes source based on a supplied id.
+
+        :param session: The session object.
+        :type session: object
+        :param id: The ID of an integration source
+        :type id: string
+        :return: Success/Failure.
+        :rtype: status code
+        """
         uri = f'/content/api/sources/{id}'
         return session._request(f'{session.baseurl}{uri}',
                                 request_method='DELETE'
